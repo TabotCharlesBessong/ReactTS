b5a79e251b77b78647fd012167065c8bdbd46a17
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-01-18 08:44:59 +0100

    styles

--- shopingCart/src/main.ts
+++ shopingCart/src/main.ts
@@ -1,23 +1,14 @@
-import './style.css'
+// import './style.css'
 import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import { Wrapper, StyledButton } from './main.styles'
+import { Grid,Badge,LinearProgress,Drawer } from '@material-ui/core'
+import { AddShoppingCart } from '@material-ui/icons'
+
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
+  <Wrapper>
+    <Drawer anchor='right' onClose={} >
+      <h1>Hello my world</h1>
+    </Drawer>
+  </Wrapper>
 `
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
