ea701fdb7db62efcd987fe393c5344ce7d16e2b3
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-01-19 21:06:56 +0100

    fetching products with react-query

--- react-shopping-cart/src/App.tsx
+++ react-shopping-cart/src/App.tsx
@@ -3,9 +3,29 @@ import {Wrapper,StyledButton} from './App.styles'
 import {Drawer,Grid,LinearProgress,Badge} from '@material-ui/core'
 import {AddShoppingCart} from '@material-ui/icons'
 import {useState} from 'react'
+import {useQuery} from 'react-query'
+
+export type CartItemType = {
+  id:number;
+  category:string;
+  description:string;
+  image:string;
+  price:number;
+  title:string;
+  amount:number
+}
+
+const getProducts = async (): Promise<CartItemType[]> => await (await fetch('https://fakestoreapi.com/products')).json()
 
 function App() {
   const [cartOpen, setCartOpen] = useState(false);
+  // const [cartItems,setCartItems] = useState([] as CartItemType[])
+  const {data,isLoading,error} = useQuery<CartItemType[]>('products',getProducts)
+  console.log(data)
+
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>Something went wrong ...</div>;
+
   return (
     <Wrapper>
       <Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false)} >
--- react-shopping-cart/src/index.tsx
+++ react-shopping-cart/src/index.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {QueryClient,QueryClientProvider} from 'react-query'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+const client = new QueryClient()
+
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={client} >
+      <App/>
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
