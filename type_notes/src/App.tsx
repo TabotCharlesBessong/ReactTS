51894806ef548da87fcd63bfd0126f73c7ab602f
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-02-19 11:12:55 +0100

    navbar

--- type_notes/src/App.tsx
+++ type_notes/src/App.tsx
@@ -1,15 +1,38 @@
-import { useState } from 'react'
-import { BrowserRouter as Router , Route,Routes } from 'react-router-dom'
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Navbar } from "./components";
+import { SelectedPage } from "./shared/types";
 
 function App() {
+	const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+		SelectedPage.Home
+	);
+	const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
 
-  return (
-    <Router>
-      <Routes>
-        <Route path='/' element='Hello' />
-      </Routes>
-    </Router>
-  )
+	useEffect(() => {
+		const handleScroll = () => {
+			if (window.scrollY === 0) {
+				setIsTopOfPage(true);
+				setSelectedPage(SelectedPage.Home);
+			}
+			if (window.scrollY !== 0) setIsTopOfPage(false);
+		};
+		window.addEventListener("scroll", handleScroll);
+		return () => window.removeEventListener("scroll", handleScroll);
+	}, []);
+
+	return (
+		<Router>
+			<Navbar
+				isTopOfPage={isTopOfPage}
+				selectedPage={selectedPage}
+				setSelectedPage={setSelectedPage}
+			/>
+			<Routes>
+				<Route path="/" element="" />
+			</Routes>
+		</Router>
+	);
 }
 
-export default App
+export default App;
