9254fe456b74f3888eceb55a306aa787ff702874
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-02-19 11:45:14 +0100

    images

---
+++ type_notes/src/constants/images.ts
@@ -0,0 +1,42 @@
+
+import wave from '../assets/AbstractWaves.png'
+import arrow from '../assets/Arrow.png'
+import benefits from '../assets/BenefitsPageGraphic.png'
+import circle from '../assets/Circles.png'
+import contact from '../assets/ContactUsPageGraphic.png'
+import evolve from '../assets/EvolveText.png'
+import homeG from '../assets/HomePageGraphic.png'
+import homeT from '../assets/HomePageText.png'
+import image1 from '../assets/image1.png'
+import image2 from '../assets/image2.png'
+import image3 from '../assets/image3.png'
+import image4 from '../assets/image4.png'
+import image5 from '../assets/image5.png'
+import image6 from '../assets/image6.png'
+import spark from '../assets/Sparkles.png'
+import sponsorForbes from '../assets/SponsorForbes.png'
+import sponsorFortune from '../assets/SponsorFortune.png'
+import sponsorRedBull from "../assets/SponsorRedBull.png"
+
+const images = {
+	wave,
+	arrow,
+	benefits,
+	circle,
+	contact,
+	evolve,
+	homeG,
+	homeT,
+	image1,
+	image2,
+	image3,
+	image4,
+	image5,
+	image6,
+  spark,
+  sponsorForbes,
+  sponsorFortune,
+  sponsorRedBull
+};
+
+export default images
